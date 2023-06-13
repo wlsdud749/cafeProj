260251668558d1b815b9e019d80cd1554cfad3eb
진영\sth74 <https://github.com/wlsdud749>
2023-06-13 23:40:14 +0900

    [MODIFY] 게시판 기능별로 페이지 구분 및 페이지 상세보기 기능 구현 중

--- src/main/java/com/example/cafe/Controller/ViewsController.java
+++ src/main/java/com/example/cafe/Controller/ViewsController.java
@@ -1,9 +1,13 @@
 package com.example.cafe.Controller;
 
+import com.example.cafe.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -13,6 +17,14 @@ public class ViewsController {
 
     @GetMapping(value = "/login")
     public void loginProc() {
+
+    }
+
+    @GetMapping(value = "/boardWrite")
+    public void boardWriteProc(HttpServletRequest req) {
+
+        HttpSession session = req.getSession();
+        UserDto dto = (UserDto) session.getAttribute("userinfo");
 
     }
 
